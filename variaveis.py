2b565720ae5ec4e5dfcfeaec1c964b4753fd7f08
OkBrahma <bennydollmoon@gmail.com>
2024-07-03 12:16:09 -0300

    Add files via upload

---
+++ variaveis.py
@@ -0,0 +1,12 @@
+a = 20
+b = 35
+
+soma = a + b
+subtração = a - b
+multiplicação = a * b
+divisão = a / b
+
+print("Resultado da soma: " + str(soma))
+print("Resultado da subtração: " + str(subtração))
+print("Resultado da multiplicação: " +str(multiplicação))
+print("Resultado da divisão: " + str(divisão))
